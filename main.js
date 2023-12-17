4e955a1ab9ce9ddce2c5915440343c0bb1c9516b
Mc-Knight <tetobobo1@gmail.com>
2023-12-18 01:51:30 +0200

    mediaq

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ const currentYear = new Date().getFullYear();
 const birthYear = 2000;
 const age = currentYear - birthYear;
 span.textContent = age;
+
+//footer yera
+const yearSpan = document.getElementById("footer-year");
+yearSpan.textContent = currentYear;
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbz00IMAUfMt0LyM3OJ2dOxJeNeWY9XgKG7XHvJ45to9tDTiq2FgIi5pzvvlaKNM2gCSDg/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
