909e4b5a9d6c9ded22a11e3da084a5d71635b7d7
Mc-Knight <tetobobo1@gmail.com>
2024-02-23 01:09:48 +0200

    like, increaseViewNumber and comment on a blog

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ form.addEventListener("submit", (e) => {
       msg.innerHTML = "Message sent successfully";
       setTimeout(() => {
         msg.innerHTML = "";
-      }, 5000);
+      }, 3000);
       form.reset();
       console.log(response);
     })
@@ -60,12 +60,34 @@ const loadLastTwoBlogs = () => {
     const noBlogs = document.createElement("p");
     noBlogs.classList.add("no-blog");
     noBlogs.textContent = "no blogs at the moment";
-    blogContainer.appendChild(p);
+    blogContainer.appendChild(noBlogs);
   } else {
     let lastTwoBlogs = blogs.slice(-2);
     for (let i = 0; i < lastTwoBlogs.length; i++) {
       const blogCard = document.createElement("div");
       blogCard.classList.add("blog-content");
+      const likedHeart = `
+      <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18 13.2222C19.49 11.6 21 9.65556 21 7.11111C21 5.49034 20.4205 3.93596 19.3891 2.7899C18.3576 1.64385 16.9587 1 15.5 1C13.74 1 12.5 1.55556 11 3.22222C9.5 1.55556 8.26 1 6.5 1C5.04131 1 3.64236 1.64385 2.61091 2.7899C1.57946 3.93596 1 5.49034 1 7.11111C1 9.66667 2.5 11.6111 4 13.2222L11 21L18 13.2222Z" fill="#F70000" stroke="#F70000" stroke-opacity="0.6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>`;
+
+      const notLikedHeart = `
+      <svg
+      width="20"
+      height="17"
+      viewBox="0 0 20 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.3 10.1667C17.641 8.95 19 7.49167 19 5.58333C19 4.36776 18.4785 3.20197 17.5502 2.34243C16.6219 1.48289 15.3628 1 14.05 1C12.466 1 11.35 1.41667 10 2.66667C8.65 1.41667 7.534 1 5.95 1C4.63718 1 3.37813 1.48289 2.44982 2.34243C1.52152 3.20197 1 4.36776 1 5.58333C1 7.5 2.35 8.95833 3.7 10.1667L10 16L16.3 10.1667Z"
+        stroke="black"
+        stroke-opacity="0.6"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>`;
 
       blogCard.innerHTML = `
       <img src="../posters/${lastTwoBlogs[i].poster}" alt="" />
@@ -78,22 +100,7 @@ const loadLastTwoBlogs = () => {
       <div class="blog-buttons">
         <div class="blog-like">
           <div>
-            <svg
-              width="20"
-              height="17"
-              viewBox="0 0 20 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16.3 10.1667C17.641 8.95 19 7.49167 19 5.58333C19 4.36776 18.4785 3.20197 17.5502 2.34243C16.6219 1.48289 15.3628 1 14.05 1C12.466 1 11.35 1.41667 10 2.66667C8.65 1.41667 7.534 1 5.95 1C4.63718 1 3.37813 1.48289 2.44982 2.34243C1.52152 3.20197 1 4.36776 1 5.58333C1 7.5 2.35 8.95833 3.7 10.1667L10 16L16.3 10.1667Z"
-                stroke="black"
-                stroke-opacity="0.6"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            ${lastTwoBlogs[i].isLiked ? likedHeart : notLikedHeart}
             ${lastTwoBlogs[i].likes}
           </div>
           <div>
