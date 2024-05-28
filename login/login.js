7d70bc250beb21f4e86e8a94b9ed4f954371dc7f
Mc-Knight <tetobobo1@gmail.com>
2024-05-28 18:14:08 +0200

    default view

--- login/login.js
+++
@@ -1,6 +0,0 @@
-const loginBtn = document.getElementById("login");
-const form = document.forms["login-continue-form"];
-
-loginBtn.addEventListener("click", () => {
-  window.location.href = "/dashboard/dashboard.html";
-});
